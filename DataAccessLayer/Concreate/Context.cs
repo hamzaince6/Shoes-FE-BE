66a77f42b366b44f57f21419a77702f1feb032f1
Hamza ince <hamzaince10@gmail.com>
2023-10-21 23:12:23 +0300

    21.10.2023

--- DataAccessLayer/Concreate/Context.cs
+++ DataAccessLayer/Concreate/Context.cs
@@ -9,21 +9,13 @@ public class Context: DbContext
     {
         optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB;database=ShoesDb; integrated security=true;");
     }
-
     public DbSet<About> Abouts { get; set; }
-
     public DbSet<Category> Categories { get; set; }
-
     public DbSet<ContackForm> ContackForms { get; set; }
-
     public DbSet<Customers> Customrs { get; set; }
-
     public DbSet<Main> Mains { get; set; }
-
     public DbSet<MainSocial> MainSocials { get; set; }
-
     public DbSet<Products> Products { get; set; }
-
     public DbSet<Services> Services { get; set; }
    
 }   
