3c49114d9d58b1aeb692eb750e80195b97cf5740
Hamza ince <hamzaince10@gmail.com>
2023-10-23 16:20:32 +0300

    BussinesLayer Edit

---
+++ BusinessLayer/Abstract/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concreate;
+
+
+namespace BusinessLayer.Abstract;
+
+public interface ICategoryService
+{
+    void CategoryAdd(Category category);
+
+    void CategoryDelete(Category category);
+
+    void CategoryUpdate(Category category);
+    
+    List<Category> Getlist();
+
+    Category GetById(int id);
+    
+}
---
+++ BusinessLayer/Concreate/CategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concreate;
+
+namespace BusinessLayer.Concreate;
+
+public class CategoryManager: ICategoryService
+{
+    public void CategoryAdd(Category category)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CategoryDelete(Category category)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CategoryUpdate(Category category)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Category> Getlist()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Category GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
