2961fecae28a8535eeb173cebfce27df35cee324
Hamza ince <hamzaince10@gmail.com>
2023-10-23 11:34:30 +0300

    23.10.20232

---
+++ DataAccessLayer/Repositories/CustomersRepository.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer.Abstract;
+
+namespace DataAccessLayer.Repositories;
+
+public class CustomersRepository : IGenericDal<CustomersRepository>
+{
+    public void Insert(CustomersRepository t)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(CustomersRepository t)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(CustomersRepository t)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<CustomersRepository> GetListAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public CustomersRepository GetByID(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concreate;
+
+namespace DataAccessLayer.Repositories;
+
+public class GenericRepository<T>: IGenericDal <T> where T: class 
+{
+    public void Insert(T t)
+    {
+        using var c = new Context();
+        c.Add(t);
+        c.SaveChanges();
+    }
+
+    public void Delete(T t)
+    {
+        using var c = new Context();
+        c.Remove(t);
+        c.SaveChanges();
+    }
+
+    public void Update(T t)
+    {
+        using var c = new Context();
+        c.Update(t);
+        c.SaveChanges();
+    }
+
+    public List<T> GetListAll()
+    {
+        using var c = new Context();
+        return c.Set<T>().ToList();
+    }
+
+    public T GetByID(int id)
+    {
+        using var c = new Context();
+        return c.Set<T>().Find(id);
+    }
+}
