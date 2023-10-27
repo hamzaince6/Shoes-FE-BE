47e0866e8360e6dea56466b727bf4d57c435ca56
Hamza ince <hamzaince10@gmail.com>
2023-10-27 10:34:00 +0300

    EntityFramework

---
+++ DataAccessLayer/EntityFramework/EfProductsRepository.cs
@@ -0,0 +1,10 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concreate;
+
+namespace DataAccessLayer.EntityFramework;
+
+public class EfProductsRepository: GenericRepository<Products>, IProductsDal
+{
+    
+}
